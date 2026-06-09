26da5ec7ea34c3f90d866535aba7b6520b804b13
CodeQueen-AI <codeq209@gmail.com>
2026-06-09 19:33:08 +0500

    update

--- app/page.tsx
+++ app/page.tsx
@@ -5,11 +5,8 @@ import Experience from "./components/Experience";
 import Skill from "./components/Skills";
 import Projects from "./components/Projects";
 import Banner from "./components/Banner";
-import Banner1 from "./components/Banner1";
 import Services from "./components/Services";
-import Hirebanner from "./components/Hirebanner";
 import ScrollStack from "./components/scrollstack";
-import ValueSection from "./components/valuesection";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
 
@@ -22,11 +19,8 @@ export default function Page() {
       <Skill />
       <Projects />
       <Banner />
-      <Banner1 />
       <Services />
-      <Hirebanner />
       <ScrollStack />
-      <ValueSection />
       <Contact />
       <Footer />
     </PageWrapper>
